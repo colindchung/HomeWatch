c2429c94e4a621a8d3eb811f04b1c69a00d15eda
Mahad Zaryab <mahadzaryab1@gmail.com>
2019-09-14 00:58:43 -0400

    Added boiler plate and stack navigator

--- userInterface/App.js
+++ userInterface/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World!!!</Text>
-  
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FF0000',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Home: Login
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'App'
+    }
   }
+);
 
-});
+export default createAppContainer(navigator);
---
+++ userInterface/screens/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+
+const Login = () => {
+    return <Text>Hello World!</Text>
+}
+
+const styles = StyleSheet.create({});
+
+export default Login;
