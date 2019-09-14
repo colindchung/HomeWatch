301116c4711e4a33588e789074fa6634abb5f312
Mahad Zaryab <mahadzaryab1@gmail.com>
2019-09-14 09:46:07 -0400

    New project file structure

--- userInterface/App.js
+++ userInterface/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from './src/screens/HomeScreen';
+import ComponentsScreen from './src/screens/ComponentsScreen';
+import ListScreen from './src/screens/ListScreen';
+import ImageScreen from './src/screens/ImageScreen';
+import CounterScreen from './src/screens/CounterScreen';
 
 const navigator = createStackNavigator(
   {
-    Home: Login,
+    Home: HomeScreen,
+    Components: ComponentsScreen,
+    List: ListScreen,
+    Image: ImageScreen,
+    Counter: CounterScreen
   },
   {
     initialRouteName: 'Home',
---
+++ userInterface/src/screens/ColorScreen.js
@@ -0,0 +1,10 @@
+import React from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
+
+const ColorScreen = () => {
+
+}
+
+const styles = StyleSheet.create({})
+
+export default ColorScreen;
