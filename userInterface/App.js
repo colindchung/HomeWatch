7a2e8dadc37304c5323d07ffaa75d5eafcd69a96
Mahad Zaryab <mahadzaryab1@gmail.com>
2019-09-14 10:33:19 -0400

    New boilerplate project

--- userInterface/App.js
+++ userInterface/App.js
@@ -1,20 +1,12 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-import HomeScreen from './src/screens/HomeScreen';
-import ComponentsScreen from './src/screens/ComponentsScreen';
-import ListScreen from './src/screens/ListScreen';
-import ImageScreen from './src/screens/ImageScreen';
-import CounterScreen from './src/screens/CounterScreen';
+import Login from './src/screens/Login';
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen,
-    Components: ComponentsScreen,
-    List: ListScreen,
-    Image: ImageScreen,
-    Counter: CounterScreen
+    LoginPage: Login,
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'LoginPage',
     defaultNavigationOptions: {
       title: 'App'
     }
---
+++ userInterface/src/screens/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+
+const Login = () => {
+    return <Text>Hello World</Text>
+};
+
+const styles = StyleSheet.create({});
+
+export default Login;
+
