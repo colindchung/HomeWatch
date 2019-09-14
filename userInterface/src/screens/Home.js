b9d2ee858cb17f93e3d583aeac64a1b64141563c
Colin Chung <colin.d.chung@gmail.com>
2019-09-14 13:30:47 -0400

    base layout

--- userInterface/src/screens/Home.js
+++ userInterface/src/screens/Home.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, 
+        Text, 
+        StyleSheet, 
+        Button,
+        Image
+    } from 'react-native';
 
 
 const Home = () => {
-    return <Text>HomePage</Text>
+    return (
+        <View style={styles.background}>
+            <View style={styles.imageBorder}>
+                <Image
+                    style={styles.stream}
+                    source={require('../../assets/tee.jpg')}
+                />
+            </View>
+            <View style={styles.imageBorder}>
+                <Image
+                    style={styles.stream}
+                    source={require('../../assets/tee2.jpeg')}
+                />
+            </View>
+        </View>
+    )
 };
 
 const styles = StyleSheet.create({
-    
+    background: {
+        flex: 1,
+        alignSelf: 'center',
+        backgroundColor: '#FFFFFF'
+    },
+    imageBorder: {
+        maxWidth: 300,
+        paddingTop: 70,
+        backgroundColor: '#FFFFFF'
+    },
+    stream: {
+        paddingTop: 60,
+        width: 200,
+        height: 200,
+        alignSelf: 'center'
+    }
 });
 
 export default Home;
