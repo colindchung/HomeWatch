cde1112f9bc5433d58c9e626e4aafcf777efc120
Mahad Zaryab <mahadzaryab1@gmail.com>
2019-09-14 21:40:48 -0400

    Connected sign up page to firebase

--- userInterface/src/screens/Register.js
+++ userInterface/src/screens/Register.js
@@ -1,15 +1,20 @@
-import React, {Component} from 'react';
-import {View, 
-        Text, 
-        TextInput,
-        StyleSheet, 
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    StyleSheet,
 
-    } from 'react-native';
-import AwesomeButton from 'react-native-really-awesome-button'
+} from 'react-native';
+import AwesomeButton from 'react-native-really-awesome-button';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+import Button from 'react-native-really-awesome-button';
+
 
 export default class Register extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             firstName: '',
@@ -17,20 +22,42 @@ export default class Register extends Component {
             password: '',
             email: '',
             phone: ''
-        }   
+        }
     }
 
-    onType(fieldName, fieldValue){
+    onType(fieldName, fieldValue) {
         this.setState({
             [fieldName]: fieldValue
         })
     }
 
-    onSubmit(){
-        // not functional yet
+    onSubmit() {
+        const { navigate } = this.props.navigation;
+        try {
+            console.log('here');
+            firebase
+                .auth()
+                .createUserWithEmailAndPassword(this.state.email, this.state.password)
+                .then(res => {
+                    console.log('firebase');
+                    firebase
+                        .firestore()
+                        .collection('users')
+                        .doc(res.user.uid)
+                        .set(
+                            { email: this.state.email,
+                            FirstName: this.state.firstName,
+                            LastName: this.state.lastName,
+                            phone: this.state.phone,
+                         });
+                });
+        }
+        catch (error) {
+            console.log('error');
+        }
     }
 
-    getTextInputStyle(wid, mgn_right, mgn_left){
+    getTextInputStyle(wid, mgn_right, mgn_left) {
         return {
             backgroundColor: '#D3D3D3',
             color: '#000000',
@@ -47,44 +74,73 @@ export default class Register extends Component {
         }
     }
 
+    print(){
+        console.log('Hello World!');
+    }
     render() {
         return (
             <View style={styles.background}>
                 <Text style={styles.header}>Please Enter Credentials</Text>
                 <View style={styles.rowAlign}>
-                    <TextInput 
+                    <TextInput
                         style={this.getTextInputStyle(140, 10, 0)}
                         placeholder='First Name'
-                        onChangeText={value => this.onType('firstname', value)}
+                        onChangeText={value => this.onType('firstName', value)}
                     />
-                    <TextInput 
+                    <TextInput
                         style={this.getTextInputStyle(140, 0, 10)}
                         placeholder='Last Name'
-                        onChangeText={value => this.onType('lastname', value)}
+                        onChangeText={value => this.onType('lastName', value)}
                     />
                 </View>
-                <TextInput 
+                <TextInput
                     style={this.getTextInputStyle(300, 0, 0)}
                     placeholder='Email Address'
                     onChangeText={value => this.onType('email', value)}
                 />
-                <TextInput 
+                <TextInput
                     style={this.getTextInputStyle(300, 0, 0)}
                     placeholder='Password'
                     onChangeText={value => this.onType('password', value)}
                 />
-                <TextInput 
+                <TextInput
                     style={this.getTextInputStyle(300, 0, 0)}
                     placeholder='Phone Number'
                     onChangeText={value => this.onType('phone', value)}
                 />
-                <AwesomeButton
-                    onPress={this.onSubmit}
+                <AwesomeButton type="primary"
+                    onPress={() => {
+                        const { navigate } = this.props.navigation;
+                        try {
+                            console.log('here');
+                            firebase
+                                .auth()
+                                .createUserWithEmailAndPassword(this.state.email, this.state.password)
+                                .then(res => {
+                                    console.log('firebase');
+                                    firebase
+                                        .firestore()
+                                        .collection('users')
+                                        .doc(res.user.uid)
+                                        .set(
+                                            { email: this.state.email,
+                                            FirstName: this.state.firstName,
+                                            LastName: this.state.lastName,
+                                            phone: this.state.phone,
+                                         });
+
+                                         navigate('HomePage')
+                                });
+                        }
+                        catch (error) {
+                            console.log('error');
+                        }
+                    }}
                 >Sign Up!</AwesomeButton>
             </View>
         )
     }
-    
+
 };
 
 const styles = StyleSheet.create({
